07d4fc74558c7ed2e4ca4935b8bca7c9a4ffaac4
Vlad Gluzman Peregrine <vlad.gluzman.peregrine@tamr.com>
2016-10-07 00:40:08 -0400

    Give the cgroup more headroom over the JVM heap.

--- scheduler/src/test/java/org/apache/mesos/elasticsearch/scheduler/configuration/ExecutorEnvironmentalVariablesTest.java
+++ scheduler/src/test/java/org/apache/mesos/elasticsearch/scheduler/configuration/ExecutorEnvironmentalVariablesTest.java
@@ -18,7 +18,24 @@ public class ExecutorEnvironmentalVariablesTest {
     private Configuration configuration = Mockito.mock(Configuration.class);
 
     @Test
-    public void ensureOver1GBHeapIs256MB() throws Exception {
+    public void ensureOver4GBHeapIs1024MB() throws Exception {
+        int ram = 4096;
+        Mockito.when(configuration.getMem()).thenReturn((double) ram);
+        ExecutorEnvironmentalVariables env = new ExecutorEnvironmentalVariables(configuration);
+
+        for (Protos.Environment.Variable var : env.getList()) {
+            if (var.getName().equals(ExecutorEnvironmentalVariables.ES_HEAP)) {
+                String val = var.getValue();
+                Pattern pattern = Pattern.compile("(\\d*)m");
+                Matcher matcher = pattern.matcher(val);
+                assertTrue(matcher.matches());
+                assertEquals(Integer.toString(ram - 1024), matcher.group(1));
+            }
+        }
+    }
+
+    @Test
+    public void ensureUnder4GBIsLessThan256MB() throws Exception {
         int ram = 2048;
         Mockito.when(configuration.getMem()).thenReturn((double) ram);
         ExecutorEnvironmentalVariables env = new ExecutorEnvironmentalVariables(configuration);
@@ -29,25 +46,8 @@ public class ExecutorEnvironmentalVariablesTest {
                 Pattern pattern = Pattern.compile("(\\d*)m");
                 Matcher matcher = pattern.matcher(val);
                 assertTrue(matcher.matches());
-                assertEquals(Integer.toString(ram - 256), matcher.group(1));
-            }
-        }
-    }
-
-    @Test
-    public void ensureUnder1GBIsLessThan256MB() throws Exception {
-        int ram = 512;
-        Mockito.when(configuration.getMem()).thenReturn((double) ram);
-        ExecutorEnvironmentalVariables env = new ExecutorEnvironmentalVariables(configuration);
-
-        for (Protos.Environment.Variable var : env.getList()) {
-            if (var.getName().equals(ExecutorEnvironmentalVariables.ES_HEAP)) {
-                String val = var.getValue();
-                Pattern pattern = Pattern.compile("(\\d*)m");
-                Matcher matcher = pattern.matcher(val);
-                assertTrue(matcher.matches());
                 assertEquals(Integer.toString(ram - ram / 4), matcher.group(1));
-                assertTrue(ram - Integer.parseInt(matcher.group(1)) < 256);
+                assertTrue(ram - Integer.parseInt(matcher.group(1)) < 1024);
                 assertTrue(ram - Integer.parseInt(matcher.group(1)) < ram);
                 assertTrue(ram - Integer.parseInt(matcher.group(1)) > 0);
             }
